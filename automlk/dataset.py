fixed problem on classification with numerical target

automlk/dataset.py
@@ -833,10 +833,11 @@ def __prepare_y(dataset, y, y_train, y_test):
     # pre-processing of y: categorical
     if dataset.problem_type == 'classification':
         # encode class values as integers
-        map_y = {x: i for i, x in enumerate(dataset.y_class_names)}
-        y = np.array([map_y[x] if x in map_y else 0 for x in y])
-        y_train = np.array([map_y[x] if x in map_y else 0 for x in y_train])
-        y_test = np.array([map_y[x] if x in map_y else 0 for x in y_test])
+        print(dataset.y_class_names)
+        map_y = {str(x): i for i, x in enumerate(dataset.y_class_names)}
+        y = np.array([map_y[str(x)] if str(x) in map_y else 0 for x in y])
+        y_train = np.array([map_y[str(x)] if str(x) in map_y else 0 for x in y_train])
+        y_test = np.array([map_y[str(x)] if str(x) in map_y else 0 for x in y_test])
     return y, y_train, y_test
 
 

automlk/worker.py
@@ -137,7 +137,8 @@ def __search(dataset, context, solution, model, msg_search, ds, pool, pipeline):
     # generate submit file
     if dataset.filename_submit != '':
         ls = len(ds.id_submit)
-        if dataset.problem_type == 'regression':
+        #if dataset.problem_type == 'regression':
+        if np.shape(y_pred_submit)[1] == 1:
             submit = np.concatenate((np.reshape(ds.id_submit, (ls, 1)), np.reshape(y_pred_submit, (ls, 1))), axis=1)
         else:
             submit = np.concatenate((np.reshape(ds.id_submit, (ls, 1)), np.reshape(y_pred_submit[:, 1], (ls, 1))), axis=1)